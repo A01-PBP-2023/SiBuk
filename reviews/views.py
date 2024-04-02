dd57f4a6f9e1dcfcd8d13c2b722f774465958006
Mikhael <midebar@gmail.com>
2024-04-02 08:59:09 +0700

    Added return json for reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseBadRequest, HttpResponseRedirect
+from django.http import HttpResponseBadRequest, HttpResponseRedirect, JsonResponse
 from django.contrib.contenttypes.models import ContentType
+from django.core import serializers
+from django.urls import reverse
 
 from .models import Review
-from foods.models import Foods
-from drinks.models import Drinks
+from foods.models import Foods # Change this
+from drinks.models import Drinks # Change this
 
 def review_FnD(request, content_type_id, object_id):
     # Get the content type and object based on IDs
@@ -25,6 +27,17 @@ def review_FnD(request, content_type_id, object_id):
             content_type=content_type,
             object_id=object_id
         )
-        return HttpResponseRedirect('/')
-    
+        if content_type.model == 'foods':
+            return HttpResponseRedirect(reverse('foods:main')) # Change this
+        else:
+            return HttpResponseRedirect(reverse('drinks:main')) # Change this
+        
     return render(request, 'review_form.html', {'object': obj})
+
+def get_reviews_json(request, content_type_id, object_id):
+    content_type = get_object_or_404(ContentType, pk=content_type_id)
+    if (content_type.model != 'foods') or (content_type.model != 'drinks'):
+        return HttpResponseBadRequest("Invalid content type")
+    reviews = Review.objects.filter(content_type=content_type, object_id=object_id)
+    reviews_data = serializers.serialize('json', reviews)
+    return JsonResponse({'reviews': reviews_data}, safe=False)
