1ffbfca02276f4d4ef8cefcb47936ada067e5901
Mikhael <midebar@gmail.com>
2024-04-02 08:18:08 +0700

    hhh

--- reviews/views.py
+++ reviews/views.py
@@ -9,9 +9,9 @@ from drinks.models import Drinks
 def review_FnD(request, content_type_id, object_id):
     # Get the content type and object based on IDs
     content_type = get_object_or_404(ContentType, pk=content_type_id)
-    if content_type.model == 'food':
+    if content_type.model == 'foods':
         obj = get_object_or_404(Foods, pk=object_id)
-    elif content_type.model == 'drink':
+    elif content_type.model == 'drinks':
         obj = get_object_or_404(Drinks, pk=object_id)
     else:
         return HttpResponseBadRequest("Invalid content type")
