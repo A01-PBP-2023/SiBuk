f450124e1eab9d40313fec1d90744030e8a225b2
A. Nurcahaya Tampubolon <anurcahayatampubolon@gmail.com>
2024-04-08 19:20:03 +0700

    rm env

--- foods/forms.py
+++ foods/forms.py
@@ -3,7 +3,7 @@ from .models import Food
 
 class FoodFilterForm(forms.Form):
     category = forms.ChoiceField(choices=Food.FOOD_CHOICES, required=False)
-    merchant_area = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder': 'Merchant Area'}))
+    merchant_area = forms.CharField(required=False)
 
 class AddFoodForm(forms.ModelForm):
     class Meta:
@@ -11,6 +11,7 @@ class AddFoodForm(forms.ModelForm):
         fields = ['Merchant_area', 'Category', 'Product_name', 'Product_description', 'Image']
         widgets = {
             'Product_name': forms.TextInput(attrs={'placeholder': 'Product Name'}),
+            'Merchant_area': forms.TextInput(attrs={'placeholder': 'Merchant Area'}),
             'Product_description': forms.TextInput(attrs={'placeholder': 'Product Description'}),
             'Image': forms.URLInput(attrs={'placeholder': 'Image URL'}),
         }
