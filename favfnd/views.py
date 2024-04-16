33acde6445aa93e592205d749bfacee7c8eb82b6
GistelaS <gistelasinurat@gmail.com>
2024-04-16 15:04:24 +0700

    editfavfnd

--- favfnd/views.py
+++ favfnd/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def show_favorites(request):
+    return render(request, 'favorites.html')
--- reviews/urls.py
+++ reviews/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import review_FnD
+# from .views import review_FnD
 
 app_name = "reviews"
 
 urlpatterns = [
     # The URL pattern includes placeholders for content type ID and object ID
-    path('', review_FnD, name='review_FnD'),
+    # path('', review_FnD, name='review_FnD'),
 ]
