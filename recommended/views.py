58eb5117fc00c145600b425f958810d521402936
Muhammad Eagel Triutama <muhammadeagel@gmail.com>
2024-04-22 10:01:47 +0700

    test recommended

--- recommended/views.py
+++ recommended/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-
+from foods.models import Food
+from django.http import HttpResponse
+from django.core import serializers
 # Create your views here.
 
 def show_page (request):
-    return render(request, 'index.html')
+    foods = Food.objects.all()[:10]
+
+    context = {
+        'data': foods
+    }
+    return render(request, 'index.html', context)
+
+
+def show_json (request) :
+    data = Food.objects.all()[:10]
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+
+
+
