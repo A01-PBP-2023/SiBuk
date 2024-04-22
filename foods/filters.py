bd7fada2e43a76071fcac8af57bb86b8be40d505
A. Nurcahaya Tampubolon <anurcahayatampubolon@gmail.com>
2024-04-22 16:20:39 +0700

    fix card food

--- foods/filters.py
+++ foods/filters.py
@@ -3,8 +3,7 @@ import django_filters
 
 class FoodFilter(django_filters.FilterSet):
     category = django_filters.ChoiceFilter(choices=Food.FOOD_CHOICES, label='Category')
-    merchant_area = django_filters.CharFilter(lookup_expr='icontains', label='Merchant Area')
 
     class Meta:
         model = Food
-        fields = ['category', 'merchant_area']
+        fields = ['category']
