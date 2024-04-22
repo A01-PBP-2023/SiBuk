f306fbe802801143cc603787d731cbe55c841dc5
Muhammad Eagel Triutama <muhammadeagel@gmail.com>
2024-04-22 14:02:05 +0700

    update index.html

--- recommended/views.py
+++ recommended/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from foods.models import Food
+from foods.views import food_detail
 # Create your views here.
 
 def show_page (request):
-    return render(request, 'index.html')
+    data = Food.objects.all()[:10]
+    context = {
+        'food': data
+    }
+    return render(request, 'index.html', context)
+
