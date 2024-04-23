679c418a9c4914bcae7fd322cb4b9c905d5a510a
A. Nurcahaya Tampubolon <anurcahayatampubolon@gmail.com>
2024-04-23 13:08:07 +0700

    update filter

--- foods/filters.py
+++ foods/filters.py
@@ -2,8 +2,6 @@ from .models import Food
 import django_filters
 
 class FoodFilter(django_filters.FilterSet):
-    category = django_filters.ChoiceFilter(choices=Food.FOOD_CHOICES, label='Category')
-
     class Meta:
         model = Food
         fields = ['category']
