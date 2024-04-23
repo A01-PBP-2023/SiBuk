dc8cf7974c87f0e2bb84ccf5cde0a1a97aebe8c1
A. Nurcahaya Tampubolon <anurcahayatampubolon@gmail.com>
2024-04-23 13:08:07 +0700

    update filter

--- foods/models.py
+++ foods/models.py
@@ -5,15 +5,9 @@ from django.contrib.contenttypes.fields import GenericRelation
 # Create your models here.
 class Food(models.Model):
     id = models.AutoField(primary_key=True)
-    FOOD_CHOICES = [
-        ('nasi', 'Nasi'),
-        ('snack', 'Snack'),
-        ('mie', 'Mie'),
-        ('lainnya', 'Lainnya')
-    ]
     merchant_area = models.CharField(max_length=255, null=True, blank=True)
     merchant_name = models.CharField(max_length=255, null=True, blank=True)
-    category = models.CharField(max_length=13, choices=FOOD_CHOICES, null=True, blank=True)
+    category = models.CharField(max_length=13, null=True, blank=True)
     product = models.CharField(max_length=255, null=True, blank=True)
     description = models.CharField(max_length=255, null=True, blank=True)
     reviews = GenericRelation(Review)
