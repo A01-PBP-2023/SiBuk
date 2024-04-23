9dee37a3956ae260d02321810ad52c70accb5147
A. Nurcahaya Tampubolon <anurcahayatampubolon@gmail.com>
2024-04-23 13:19:48 +0700

    update filter

--- foods/forms.py
+++ foods/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Food
 
 class FoodFilterForm(forms.Form):
-    category = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder': 'Category'}))
+    category = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder': 'Nasi/Mie/Snack/Lainnya'}))
 
 class AddFoodForm(forms.ModelForm):
     class Meta:
