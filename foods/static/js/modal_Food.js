f150468ea62bad5659f61592dc2c42df28cce638
A. Nurcahaya Tampubolon <anurcahayatampubolon@gmail.com>
2024-04-24 17:22:18 +0700

    Reapply "add page for admin"
    This reverts commit e0897d43025666f17dcec497cb6b0213307ffe21.

--- foods/static/js/modal_Food.js
+++ foods/static/js/modal_Food.js
@@ -5,3 +5,25 @@ function submitFilterForm() {
     });
     form.submit();
   }
+
+  async function showAddFoodModal() {
+    document.querySelector("#modal").classList.remove("hidden");
+  
+    document.getElementById("confirm-modal").onclick = async function () {
+        await addFood();
+        closeModal();
+    };
+  }
+
+  async function addFood() {
+    const form = new FormData(document.querySelector("#form"));
+    const response = await fetch("/foods/add_food/", {
+      method: "POST",
+      body: form,
+    });
+  
+    if (!response.ok) {
+      throw new Error("Failed to add food");
+    }
+    return false;
+  }
