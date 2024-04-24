cc038ea4ff305271bec76b3280a9a6778269990e
Muhammad Eagel Triutama <muhammadeagel@gmail.com>
2024-04-24 18:43:16 +0700

    add recommended

--- recommended/urls.py
+++ recommended/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from recommended.views import show_page
+from recommended.views import show_page, show_json
 
 app_name = 'recommended'
 
 urlpatterns = [
     path('', show_page, name='show_page'),
+    path("show_json/", show_json, name="show_json")
+
 ]
