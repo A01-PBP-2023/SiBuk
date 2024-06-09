2c91dcda0baebadc8e39d147be3cb00ea9a7668b
Nazwa Allysa <nazwa.allysa@ui.ac.id>
2024-06-09 20:05:45 +0700

    add corsheaders di settings.py

--- sibuk/settings.py
+++ sibuk/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'reviews',
     'drinks',
     'user_auth',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'sibuk.urls'
